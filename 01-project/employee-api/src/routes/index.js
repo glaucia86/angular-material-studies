928ea61109587d64aa993f3312af53bf88849021
glaucia86 <glaucia_lemos86@hotmail.com>
2022-11-28 17:20:58 -0300

    developing the main route

--- 01-project/employee-api/src/routes/index.js
+++ 01-project/employee-api/src/routes/index.js
@@ -4,3 +4,17 @@
  * date: 11/28/2022
  * author: Glaucia Lemos <@glaucia_lemos86>
  */
+
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/api/v1', (req, res) => {
+  res.status(200).send({
+    success: 'true',
+    message: 'Welcome to the Employee API!',
+    version: '1.0.0',
+  });
+});
+
+module.exports = router;
