2e4d939776e5b71a682f7cf5ba747f90e21345ed
glaucia86 <glaucia_lemos86@hotmail.com>
2022-11-28 17:03:26 -0300

    feat: developing the 'server.js' file

--- 01-project/employee-api/server.js
+++ 01-project/employee-api/server.js
@@ -4,3 +4,11 @@
  * date: 11/28/2022
  * author: Glaucia Lemos <@glaucia_lemos86>
  */
+
+const app = require('./src/app');
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
