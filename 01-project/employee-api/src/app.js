3040951177a6bc59da5b570c80f08586b618d43e
glaucia86 <glaucia_lemos86@hotmail.com>
2022-11-28 17:08:40 -0300

    feat: developing the 'app.js' file

--- 01-project/employee-api/src/app.js
+++ 01-project/employee-api/src/app.js
@@ -4,3 +4,23 @@
  * date: 11/28/2022
  * author: Glaucia Lemos <@glaucia_lemos86>
  */
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// ==> API's Routes
+const index = require('./routes/index');
+// const employeeRoute = require('./routes/employee.routes');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.json({ type: 'application/vdn.api+json' }));
+app.use(cors());
+
+app.use(index);
+// app.use('/api/v1/', employeeRoute);
+app.use('/api/v1/');
+
+module.exports = app;
