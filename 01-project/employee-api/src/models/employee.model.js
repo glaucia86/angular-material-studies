846860c2c241257ade4799218d08fde67b28b267
glaucia86 <glaucia_lemos86@hotmail.com>
2022-11-28 17:53:37 -0300

    feat: developing the 'Employee' model

--- 01-project/employee-api/src/models/employee.model.js
+++ 01-project/employee-api/src/models/employee.model.js
@@ -4,3 +4,30 @@
  * date: 11/28/2022
  * author: Glaucia Lemos <@glaucia_lemos86>
  */
+
+const mongoose = require('mongoose');
+
+/**
+ * Employee Schema
+ *
+ * firstName: String
+ * lastName: String
+ * email: String
+ * phone: Number
+ * collection: employees
+ */
+
+const employeeSchema = mongoose.Schema(
+  {
+    firstName: { type: String, required: true, maxlength: 100 },
+    lastName: { type: String, required: true, maxlength: 100 },
+    email: { type: String, required: true, maxlength: 150 },
+    phone: { type: Number, required: true, maxlength: 11 },
+  },
+  {
+    timestamps: true,
+    collection: 'employees',
+  }
+);
+
+module.exports = mongoose.model('Employee', employeeSchema);
