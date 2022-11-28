38aed858b0071d3cd3b55106460a9ed9717968cf
glaucia86 <glaucia_lemos86@hotmail.com>
2022-11-28 17:37:44 -0300

    feat: some a little changes

--- 01-project/employee-api/src/app.js
+++ 01-project/employee-api/src/app.js
@@ -16,11 +16,11 @@ const index = require('./routes/index');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.json({ type: 'application/vdn.api+json' }));
+app.use(express.json({ type: 'application/vnd.api+json' }));
 app.use(cors());
 
 app.use(index);
 // app.use('/api/v1/', employeeRoute);
-app.use('/api/v1/');
+// app.use('/api/v1/');
 
 module.exports = app;
