43063eb186e367673da580cc802ed11074d5c54e
glaucia86 <glaucia_lemos86@hotmail.com>
2022-11-29 20:25:06 -0300

    feat: defining the routes for the application

--- 01-project/client/src/app/app-routing.module.ts
+++ 01-project/client/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { EditEmployeeComponent } from './components/edit-employee/edit-employee.component';
+import { ListEmployeesComponent } from './components/list-employees/list-employees.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'add-employee' },
+  { path: 'add-employee', component: AddEmployeeComponent }
+  { path: 'edit-employee', component: EditEmployeeComponent }
+  { path: 'list-employees', component: ListEmployeesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
