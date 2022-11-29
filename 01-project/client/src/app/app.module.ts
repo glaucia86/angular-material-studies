28a552f9983b158c962eedb19e2f8fd3146c80d0
glaucia86 <glaucia_lemos86@hotmail.com>
2022-11-29 19:44:43 -0300

    feat: creating some components and service file

--- 01-project/client/src/app/app.module.ts
+++ 01-project/client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { EditEmployeeComponent } from './components/edit-employee/edit-employee.component';
+import { ListEmployeeComponent } from './components/list-employee/list-employee.component';
+import { ListEmployeesComponent } from './components/list-employees/list-employees.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddEmployeeComponent,
+    EditEmployeeComponent,
+    ListEmployeeComponent,
+    ListEmployeesComponent
   ],
   imports: [
     BrowserModule,
---
+++ 01-project/client/src/app/components/list-employees/list-employees.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-employees',
+  templateUrl: './list-employees.component.html',
+  styleUrls: ['./list-employees.component.css']
+})
+export class ListEmployeesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
