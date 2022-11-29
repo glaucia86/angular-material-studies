0d1d149996c0208a40a6c47aa59ec744c6a45d35
glaucia86 <glaucia_lemos86@hotmail.com>
2022-11-29 20:18:19 -0300

    feat: adding components to the project

--- 01-project/client/src/app/app.module.ts
+++ 01-project/client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { EditEmployeeComponent } from './components/edit-employee/edit-employee.component';
+import { ListEmployeesComponent } from './components/list-employees/list-employees.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddEmployeeComponent,
+    EditEmployeeComponent,
+    ListEmployeesComponent
   ],
   imports: [
     BrowserModule,
