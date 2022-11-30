51ac17c5cfdecd85fdfca4f0d8238aa7a841f23c
glaucia86 <glaucia_lemos86@hotmail.com>
2022-11-30 14:08:10 -0300

    feat: create 'Employee' class

---
+++ 01-project/client/src/app/shared/employee.ts
@@ -0,0 +1,15 @@
+
+/**
+ * file: src/app/shared/employee.ts
+ * description: file responsible for the 'Employee' class
+ * date: 11/30/2022
+ * author: Glaucia Lemos <@glaucia_lemos86>
+ */
+
+export class Employee {
+  _id: String;
+  firstName: String;
+  lastName: String;
+  email: String;
+  phone: Number;
+}
