91bb463cbdd01774b014f0eb0b98f98cc212e43a
glaucia86 <glaucia_lemos86@hotmail.com>
2022-11-29 21:24:53 -0300

    chore: change the angular material and cdk version

--- 01-project/client/src/app/components/add-employee/add-employee.component.ts
+++ 01-project/client/src/app/components/add-employee/add-employee.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-employee',
-  templateUrl: './add-employee.component.html',
-  styleUrls: ['./add-employee.component.css']
+  templateUrl: './add-employee.component.html'
 })
 export class AddEmployeeComponent implements OnInit {
 
--- 01-project/client/src/app/components/list-employees/list-employees.component.ts
+++ 01-project/client/src/app/components/list-employees/list-employees.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list-employees',
-  templateUrl: './list-employees.component.html',
-  styleUrls: ['./list-employees.component.css']
+  templateUrl: './list-employees.component.html'
 })
 export class ListEmployeesComponent implements OnInit {
 
