1808225c53207a7c326bf97c5026a5f5ed286c41
glaucia86 <glaucia_lemos86@hotmail.com>
2022-12-05 22:11:43 -0300

    feat: addint new modules - 'app.module.ts'

--- 01-project/client/src/app/app.module.ts
+++ 01-project/client/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,6 +29,8 @@ import { EmployeeService } from './shared/employee.service';
     BrowserAnimationsModule,
     AngularMaterialModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [EmployeeService],
   bootstrap: [AppComponent],
