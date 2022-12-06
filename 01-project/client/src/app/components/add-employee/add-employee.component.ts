809990ffa467897fa29012f4785bc998084fffdc
glaucia86 <glaucia_lemos86@hotmail.com>
2022-12-05 22:33:40 -0300

    feat: developing logic around 'add-employee.component.ts'

--- 01-project/client/src/app/components/add-employee/add-employee.component.ts
+++ 01-project/client/src/app/components/add-employee/add-employee.component.ts
@@ -1,14 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, OnInit, ViewChild, NgZone } from '@angular/core';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { EmployeeService } from 'src/app/shared/employee.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-employee',
   templateUrl: './add-employee.component.html'
 })
 export class AddEmployeeComponent implements OnInit {
-
-  constructor() { }
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  @ViewChild('chipList') chipList;
+  @ViewChild('resetEmployeeForm') myNgForm;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  employeeForm: FormGroup;
 
   ngOnInit(): void {
+    this.submitForm();
+  }
+
+  constructor(
+    public fb: FormBuilder,
+    private router: Router,
+    private ngZone: NgZone,
+    private employeeService: EmployeeService
+  ) { }
+
+  submitForm() {
+    this.employeeForm = this.fb.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      phone: ['', [Validators.pattern('^[0-9]+$')]],
+    })
+  }
+
+  public handleError = (controlName: string, errorName: string) => {
+    return this.employeeForm.controls[controlName].hasError(errorName);
+  };
+
+  submitEmployeeForm() {
+    if (this.employeeForm.valid) {
+      this.employeeService.createEmployee(this.employeeForm.value).subscribe((res) => {
+        this.ngZone.run(() => this.router.navigateByUrl('/list-employees'));
+      })
+    }
   }
 
 }
