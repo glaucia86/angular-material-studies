4868d5fbc5fd734a316d8ac28863174163497058
glaucia86 <glaucia_lemos86@hotmail.com>
2022-12-06 00:16:35 -0300

    finilizing the project

--- 01-project/client/src/app/components/edit-employee/edit-employee.component.ts
+++ 01-project/client/src/app/components/edit-employee/edit-employee.component.ts
@@ -1,14 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit, ViewChild, NgZone } from '@angular/core';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { EmployeeService } from 'src/app/shared/employee.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-employee',
   templateUrl: './edit-employee.component.html'
 })
 export class EditEmployeeComponent implements OnInit {
-
-  constructor() { }
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  @ViewChild('chipList') chipList;
+  @ViewChild('resetEmployeeForm') myNgForm;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  employeeForm: FormGroup;
 
   ngOnInit(): void {
+    this.updateForm();
+  }
+
+  constructor(
+    public fb: FormBuilder,
+    private router: Router,
+    private ngZone: NgZone,
+    private actRoute: ActivatedRoute,
+    private employeeService: EmployeeService
+  ) {
+    var id = this.actRoute.snapshot.paramMap.get('id');
+    this.employeeService.getEmployee(id).subscribe((data) => {
+      console.log(data.subjects);
+      this.employeeForm = this.fb.group({
+        firstName: [data.firstName, [Validators.required]],
+        lastName: [data.lastName, [Validators.required]],
+        email: [data.email, [Validators.required]],
+        phone: [data.phone, [Validators.required, Validators.pattern('^[0-9]+$')]],
+      });
+    });
+  }
+
+  updateForm() {
+    this.employeeForm = this.fb.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      phone: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+    });
+  }
+
+  public handleError = (controlName: string, errorName: string) => {
+    return this.employeeForm.controls[controlName].hasError(errorName);
+  };
+
+  updateEmployeeForm() {
+    console.log(this.employeeForm.value);
+    var id = this.actRoute.snapshot.paramMap.get('id');
+    if (window.confirm('Are you sure you want to update?')) {
+      this.employeeService.updateEmployee(id, this.employeeForm.value).subscribe((res) => {
+        this.ngZone.run(() => this.router.navigateByUrl('/list-employees'));
+      });
+    }
   }
 
 }
--- 01-project/client/src/app/components/list-employees/list-employees.component.ts
+++ 01-project/client/src/app/components/list-employees/list-employees.component.ts
@@ -1,14 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/shared/employee';
+import { EmployeeService } from 'src/app/shared/employee.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-list-employees',
   templateUrl: './list-employees.component.html'
 })
 export class ListEmployeesComponent implements OnInit {
+  EmployeeData: any = [];
+  dataSource: MatTableDataSource<Employee>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  displayedColumns: string[] = [
+    '_id',
+    'firstName',
+    'lastName',
+    'email',
+    'phone',
+    'action',
+  ];
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService) {
+    this.employeeService.getEmployees().subscribe((data) => {
+      this.EmployeeData = data;
+      this.dataSource = new MatTableDataSource<Employee>(this.EmployeeData);
+      setTimeout(() => {
+        this.dataSource.paginator = this.paginator;
+      }, 0);
+    });
+  }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  deleteEmployee(index: number, e: { _id: string; }) {
+    if (window.confirm('Are you sure')) {
+      const data = this.dataSource.data;
+      data.splice(
+        this.paginator.pageIndex * this.paginator.pageSize + index,
+        1
+      );
+      this.dataSource.data = data;
+      this.employeeService.deleteEmployee(e._id).subscribe();
+    }
   }
 
 }
